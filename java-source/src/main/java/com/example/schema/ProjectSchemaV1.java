69439990066eff856b8f727099a94a9c093fad7b
trivolac <brian.tan.2013@sis.smu.edu.sg>
2018-01-17 11:36:02 +0800

    Created Project State

---
+++ java-source/src/main/java/com/example/schema/ProjectSchemaV1.java
@@ -0,0 +1,99 @@
+package com.example.schema;
+
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.UniqueIdentifier;
+import net.corda.core.identity.Party;
+import net.corda.core.schemas.MappedSchema;
+import net.corda.core.schemas.PersistentState;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.UUID;
+
+public class ProjectSchemaV1 extends MappedSchema {
+    public ProjectSchemaV1(){
+        super(ProjectSchema.class, 1, ImmutableList.of(PersistentProject.class));
+    }
+
+    @Entity
+    @Table(name = "Project")
+    public static class PersistentProject extends PersistentState {
+        @Column(name = "projectCode") private final String projectCode;
+        @Column(name = "allocationKey") private final String allocationKey;
+        @Column(name = "description") private final String description;
+        @Column(name = "budget") private final int budget;
+        @Column(name = "startDate") private final Date startDate;
+        @Column(name = "endDate") private final Date endDate;
+        @Column(name = "sponsor") private final String sponsor;
+        @Column(name = "platformLead") private final String platformLead;
+        @Column(name = "cio") private final String cio;
+        @Column(name = "coo") private final String coo;
+        @Column(name = "deliveryTeam") private final String deliveryTeam;
+        @Column(name = "linearId") private final UUID linearId;
+
+        public PersistentProject(String projectCode, String allocationKey, String description, int budget, Date startDate, Date endDate, String sponsor, String platformLead, String cio, String coo, String deliveryTeam, UUID linearId) {
+            this.projectCode = projectCode;
+            this.allocationKey = allocationKey;
+            this.description = description;
+            this.budget = budget;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.sponsor = sponsor;
+            this.platformLead = platformLead;
+            this.cio = cio;
+            this.coo = coo;
+            this.deliveryTeam = deliveryTeam;
+            this.linearId = linearId;
+        }
+
+        public String getProjectCode() {
+            return projectCode;
+        }
+
+        public String getAllocationKey() {
+            return allocationKey;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public int getBudget() {
+            return budget;
+        }
+
+        public Date getStartDate() {
+            return startDate;
+        }
+
+        public Date getEndDate() {
+            return endDate;
+        }
+
+        public String getSponsor() {
+            return sponsor;
+        }
+
+        public String getPlatformLead() {
+            return platformLead;
+        }
+
+        public String getCio() {
+            return cio;
+        }
+
+        public String getCoo() {
+            return coo;
+        }
+
+        public String getDeliveryTeam() {
+            return deliveryTeam;
+        }
+
+        public UUID getLinearId() {
+            return linearId;
+        }
+    }
+}
---
+++ java-source/src/main/java/com/example/state/ProjectState.java
@@ -0,0 +1,137 @@
+package com.example.state;
+
+import com.example.schema.ProjectSchemaV1;
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.LinearState;
+import net.corda.core.contracts.UniqueIdentifier;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+import net.corda.core.schemas.MappedSchema;
+import net.corda.core.schemas.PersistentState;
+import net.corda.core.schemas.QueryableState;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class ProjectState implements LinearState, QueryableState {
+    private final String projectCode;
+    private final String allocationKey;
+    private final String description;
+    private final int budget;
+    private final Date startDate;
+    private final Date endDate;
+    private final Party sponsor;
+    private final Party platformLead;
+    private final Party cio;
+    private final Party coo;
+    private final Party deliveryTeam;
+    private final UniqueIdentifier linearId;
+
+    public ProjectState(String projectCode, String allocationKey, String description, int budget, Date startDate, Date endDate, Party sponsor, Party platformLead, Party cio, Party coo, Party deliveryTeam) {
+        this.projectCode = projectCode;
+        this.allocationKey = allocationKey;
+        this.description = description;
+        this.budget = budget;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.sponsor = sponsor;
+        this.platformLead = platformLead;
+        this.cio = cio;
+        this.coo = coo;
+        this.deliveryTeam = deliveryTeam;
+        this.linearId = new UniqueIdentifier();
+    }
+
+    public ProjectState(String projectCode, String allocationKey, String description, int budget, Date startDate, Date endDate, Party sponsor, Party platformLead, Party cio, Party coo, Party deliveryTeam, UniqueIdentifier linearId) {
+        this.projectCode = projectCode;
+        this.allocationKey = allocationKey;
+        this.description = description;
+        this.budget = budget;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.sponsor = sponsor;
+        this.platformLead = platformLead;
+        this.cio = cio;
+        this.coo = coo;
+        this.deliveryTeam = deliveryTeam;
+        this.linearId = linearId;
+    }
+
+    public String getProjectCode() {
+        return projectCode;
+    }
+
+    public String getAllocationKey() {
+        return allocationKey;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public Party getSponsor() {
+        return sponsor;
+    }
+
+    public Party getPlatformLead() {
+        return platformLead;
+    }
+
+    public Party getCio() {
+        return cio;
+    }
+
+    public Party getCoo() {
+        return coo;
+    }
+
+    public Party getDeliveryTeam() {
+        return deliveryTeam;
+    }
+
+    @NotNull
+    @Override
+    public UniqueIdentifier getLinearId() {
+        return linearId;
+    }
+
+    @NotNull
+    @Override
+    public Iterable<MappedSchema> supportedSchemas() {
+        return ImmutableList.of(new ProjectSchemaV1());
+    }
+
+    @NotNull
+    @Override
+    public PersistentState generateMappedObject(MappedSchema schema) {
+        if(schema instanceof ProjectSchemaV1){
+            UUID uuid = (this.linearId == null) ? null : this.linearId.getId();
+
+            return new ProjectSchemaV1.PersistentProject(this.projectCode, this.allocationKey, this.description, this.budget, this.startDate, this.endDate, this.sponsor.toString(), this.platformLead.toString(), this.cio.toString(), this.coo.toString(), this.deliveryTeam.toString(), uuid);
+        }else{
+            throw new IllegalArgumentException("Unrecognised schema $schema");
+        }
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return Arrays.asList(platformLead, sponsor, cio, coo, deliveryTeam);
+    }
+}
+
